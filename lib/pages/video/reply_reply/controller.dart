a45c4d903d9f7da273ed856e8524df557e8724a5
dom <githubaccount56556@proton.me>
2026-08-09 09:28:37 +0800

    fix #2491
    Signed-off-by: dom <githubaccount56556@proton.me>

--- lib/pages/video/reply_reply/controller.dart
+++ lib/pages/video/reply_reply/controller.dart
@@ -1,5 +1,5 @@
 import 'package:PiliPlus/grpc/bilibili/main/community/reply/v1.pb.dart'
-    show ReplyInfo, DetailListReply, Mode;
+    show ReplyInfo, DetailListReply;
 import 'package:PiliPlus/grpc/reply.dart';
 import 'package:PiliPlus/http/loading_state.dart';
 import 'package:PiliPlus/pages/common/publish/publish_route.dart';
@@ -50,7 +50,6 @@ class VideoReplyReplyController extends ReplyController
   @override
   void onInit() {
     super.onInit();
-    mode = Mode.MAIN_LIST_TIME;
     queryData();
   }
 
