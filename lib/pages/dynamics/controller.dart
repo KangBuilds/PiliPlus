ec5adbde34b408f780d77dcd41eebacecd46d69e
Chris Wei <kangplus7@gmail.com>
2026-08-28 13:46:15 +1200

    fix: handle missing dynamics tab controller on refresh

--- lib/pages/dynamics/controller.dart
+++ lib/pages/dynamics/controller.dart
@@ -94,8 +94,12 @@ class DynamicsController
 
   @override
   Future<void> onRefresh() {
-    singleRefresh();
-    return controller!.onRefresh();
+    final controller = this.controller;
+    if (controller != null) {
+      singleRefresh();
+      return controller.onRefresh();
+    }
+    return singleRefresh();
   }
 
   @override
